528c1770db2777f93b0d40f99503ebcd71ec7012
Pr-Najda23 <alharrasnajda23@gmail.com>
2026-07-08 12:20:55 +0100

    updated

--- dbt_project/models/gold/fact_listings.sql
+++ dbt_project/models/gold/fact_listings.sql
@@ -4,31 +4,23 @@
     tags=['gold', 'fact']
 ) }}
 
+
+
 SELECT
-    listing_id,
-    country,
-    city,
-    neighborhood,
-    property_type,
-    listing_date,
-    listing_year,
-    listing_month,
-    listing_quarter,
+    listing_id AS listing_key,                                      
+    HASH(country, city, neighborhood) AS location_key,               
+    listing_id AS property_key,                                      
+    TO_NUMBER(TO_CHAR(listing_date, 'YYYYMMDD')) AS time_key,         
     price,
     price_per_m2,
     surface_m2,
-    num_rooms,
-    num_bathrooms,
-    condition,
-    energy_rating,
-    parking,
     days_on_market,
-    CASE 
+    CASE
         WHEN price < 100000 THEN 'BUDGET'
         WHEN price < 300000 THEN 'MID'
         WHEN price < 600000 THEN 'PREMIUM'
-        ELSE 'LUXURY' 
-    END as price_segment,
-    CURRENT_TIMESTAMP() as created_at
+        ELSE 'LUXURY'
+    END AS price_segment,
+    CURRENT_TIMESTAMP() AS created_at
 
 FROM {{ ref('stg_listings') }}
